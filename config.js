cb825653314c7ac8a1f163b724bec7681bea0ff8
akansha <akanksha03051997@gmail.com>
2024-07-17 11:56:17 +0530

    config

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+   WEBSITE_URL: "http://yourwebsite.com",
+   maillerConfig: {
+     service: "Gmail",
+     auth: {
+       user: "your-email@gmail.com",
+       pass: "your-email-password"
+     }
+   }
+ };
+ 
